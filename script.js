877fa0fc95c327586d9032c3a3fca027b11f256f
CENDOC <territoriocbbr@gmail.com>
2026-01-08 19:35:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-document.querySelectorAll('.note').forEach(note => {
+document.querySelectorAll(".note").forEach(note => {
   const id = note.dataset.id;
 
-  note.style.setProperty(
-    '--rot',
-    `${Math.random() * 6 - 3}deg`
-  );
-
-  if (localStorage.getItem('note-' + id) === 'open') {
-    note.classList.add('open');
+  if (localStorage.getItem(`note-${id}`) === "open") {
+    note.classList.add("open");
   }
 
-  note.addEventListener('click', () => {
-    if (!note.classList.contains('open')) {
-      note.classList.add('open');
-      localStorage.setItem('note-' + id, 'open');
+  note.addEventListener("click", () => {
+    if (!note.classList.contains("open")) {
+      note.classList.add("open");
+      localStorage.setItem(`note-${id}`, "open");
     }
   });
 });
